927bba64a42fb6c669b2dc3a94aa3d88b2da4e2f
Cesar-Hdz_CIS <cohernan@cougarnet.uh.edu>
2021-11-30 20:15:17 -0600

    Q2

--- 12.9/main.py
+++ 12.9/main.py
@@ -1,2 +1,19 @@
 # Cesar Hernandez
 # 1835494
+
+parts = input().split() #Split input into 2
+name = parts[0] #set name from input
+names = [] #empty name list
+ages = [] #empty age list
+
+while name !='-1' : #as long as input is not -1 continue the loop
+    try:
+        age = int(parts[1]) + 1
+    except:
+        age = 0
+    names.append(name)
+    ages.append(age)
+    parts = input().split()
+    name = parts[0]
+for i in range(0,len(names)):
+    print('{} {}'.format(names[i], ages[i]))
