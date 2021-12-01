00b474c420f278e55c86f919f2c0e78986fafe5a
Cesar-Hdz_CIS <cohernan@cougarnet.uh.edu>
2021-11-30 20:30:52 -0600

    Q3

--- 14.11/main.py
+++ 14.11/main.py
@@ -1,2 +1,16 @@
 # Cesar Hernandez
 # 1835494
+
+def selection_sort_descend_trace(numbers):
+    for index in range(len(numbers) - 1):
+        largest_index = index
+        for n in range(index + 1, len(numbers)):
+            if numbers[n] > numbers[largest_index]:
+                largest_index = n
+        numbers[index], numbers[largest_index] = numbers[largest_index], numbers[index]
+        print(' '.join([str(x) for x in numbers]), "")
+    return numbers
+
+if __name__ == '__main__':
+    number_list = [int(value) for value in input().split()]
+    selection_sort_descend_trace(number_list)
